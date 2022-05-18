74d2940f3d8123d2aa890c88a6cf1a432e819ed4
mohamed9974 <mohamed.amin@metu.edu.tr>
2022-05-18 23:11:27 +0300

    All components Ready

--- mern-tracker/src/components/EditExercise.component.js
+++ mern-tracker/src/components/EditExercise.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 export default class EditExercise extends Component {
     render() {
         return (
