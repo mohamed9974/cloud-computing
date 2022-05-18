74d5c3f49231f30673ad4f3b18394e338e83353b
mohamed9974 <mohamed.amin@metu.edu.tr>
2022-05-18 22:35:17 +0300

    fixed react

--- mern-tracker/src/App.js
+++ mern-tracker/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/ExercisesList.component";
+import EditExercise from "./components/EditExercise.component";
+import CreateExercise from "./components/CreateExercise.component";
+import CreateUser from "./components/CreateUser.component";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
